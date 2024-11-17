ce679caa16726a7e1c9cb2e1d8f1783893642a0b
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-11-17 15:47:13 +0530

    [Shreesha] Added Nunit examples

---
+++ NUnitProjectTest/CheckPalindromeTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using NunitProject;
+namespace NUnitProjectTest
+{
+    [TestFixture]   
+    public class CheckPalindromeTest
+    {
+        private CheckPalindrome _checkPalindrome;
+        [SetUp]
+        public void Setup()
+        {
+            _checkPalindrome = new CheckPalindrome();
+        }
+        [Test]
+        public void WhenGivenPalindrome_ThenReturnTrue()
+        {
+            string str = "madam";
+            bool expected = true;
+            bool actual=_checkPalindrome.IsPalindrome(str);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        [TestCase("Alex",false)]
+        public void WhenGivenNonPalindrome_ThenReturnFalse(string str,bool expected)
+        {
+            bool actual= _checkPalindrome.IsPalindrome(str);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ NunitProject/CheckPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+
+namespace NunitProject
+{
+    public class CheckPalindrome
+    {
+
+        public bool IsPalindrome(string str)
+        {
+            string res = new string(str.Reverse().ToArray());
+                if (str.Equals(res,StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            CheckPalindrome p=new CheckPalindrome();
+            Console.WriteLine(p.IsPalindrome("madam"));
+        }
+    }
+}
